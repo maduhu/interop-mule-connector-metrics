2e61c6c197dfcde2c4babd2d79e2e6cc02e2fa59
Honain Khan <honaink@gmail.com>
2016-09-08 14:30:34 -0500

    debugged and fix metrics output and configuration issues

--- src/main/java/com/l1p/interop/mule/connector/metrics/config/ConnectorConfig.java
+++ src/main/java/com/l1p/interop/mule/connector/metrics/config/ConnectorConfig.java
@@ -35,31 +35,6 @@ public class ConnectorConfig {
 	@Configurable
 	private int reporterInterval;
 
-	@Configurable
-	@Summary("Enter Host")
-	@FriendlyName("Host")
-	@Placement(tab = "WebSocket Server", group = "WebSocket Server", order = 1)
-	private String host;
-
-	@Configurable
-	@Summary("Enter Port")
-	@FriendlyName("Port")
-	@Placement(tab = "WebSocket Server", group = "WebSocket Server", order = 2)
-	private int port;
-
-	@Configurable
-	@Summary("Enter Service Name")
-	@FriendlyName("Service Name")
-	@Placement(tab = "WebSocket Server", group = "WebSocket Server", order = 3)
-	private String servicename;
-
-	@Configurable
-	@Summary("Use WebSocket Server Embedded")
-	@FriendlyName("Use WebSocket Server Embedded")
-	@Default("false")
-	@Placement(tab = "WebSocket Server", group = "WebSocket Server", order = 4)
-	private boolean serverembedded;
-
 	public MetricRegistry getMetricRegistry() {
 		return metricRegistry;
 	}
@@ -82,38 +57,6 @@ public class ConnectorConfig {
 
 	public void setScheduledReporters(List<ScheduledReporter> scheduledReporters) {
 		this.scheduledReporters = scheduledReporters;
-	}
-
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public int getPort() {
-		return port;
-	}
-
-	public void setPort(int port) {
-		this.port = port;
-	}
-
-	public String getServicename() {
-		return servicename;
-	}
-
-	public void setServicename(String servicename) {
-		this.servicename = servicename;
-	}
-
-	public boolean isServerembedded() {
-		return serverembedded;
-	}
-
-	public void setServerembedded(boolean serverembedded) {
-		this.serverembedded = serverembedded;
 	}
 
 }
