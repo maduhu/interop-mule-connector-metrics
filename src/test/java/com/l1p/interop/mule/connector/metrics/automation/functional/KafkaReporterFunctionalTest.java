a29a2aff5cd09217bf987ef5b8b4f41b4df633da
Honain Khan <honaink@gmail.com>
2017-01-25 18:36:32 -0600

    Test updates

--- src/test/java/com/l1p/interop/mule/connector/metrics/automation/functional/KafkaReporterFunctionalTest.java
+++ src/test/java/com/l1p/interop/mule/connector/metrics/automation/functional/KafkaReporterFunctionalTest.java
@@ -14,19 +14,20 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 @Ignore
 public class KafkaReporterFunctionalTest {
 
-  private String kafkaTopic = "bmgf.metric.20160921";
-  private String kafkaServer = "ec2-52-26-168-223.us-west-2.compute.amazonaws.com:9092";
+  private String kafkaTopic = "bmgf.metric.pi4";
+//  private String kafkaServer = "ec2-52-26-168-223.us-west-2.compute.amazonaws.com:9092";
+  private String kafkaServer = "localhost:9092";
 
   @Test
   public void generateSomeMetrics() throws Exception {
     final MetricRegistry metricRegistry = new MetricRegistry();
     final KafkaReporter kafkaReporter = ReporterFactory.createKafkaReporter(metricRegistry, kafkaTopic, new MetricKafkaProducer(kafkaServer, "reporter-functional-test"), "ft-env", "ft-app");
-    kafkaReporter.start(3, SECONDS);
+    kafkaReporter.start(5, SECONDS);
     final ConsoleReporter consoleReporter = ReporterFactory.createConsoleReporter(metricRegistry);
-    consoleReporter.start(3, SECONDS);
+    consoleReporter.start(5, SECONDS);
 
-    final Counter counter = metricRegistry.counter("my.counter.0922");
-    final Timer timer = metricRegistry.timer("my.timer.0922");
+    final Counter counter = metricRegistry.counter("my.counter");
+    final Timer timer = metricRegistry.timer("my.timer");
 //    final Meter something = metricRegistry.meter("something");
 //    something.mark();
     final Random random = new Random(System.currentTimeMillis());
